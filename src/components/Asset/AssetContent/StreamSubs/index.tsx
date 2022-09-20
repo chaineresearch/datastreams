f6862b4fd5df4cfd5d03a1bf9474b7732f575d49
okpo <okpo.ekpenyong@gmail.com>
2022-09-20 13:12:30 +0100

    fix for netlify

--- src/components/Asset/AssetContent/StreamSubs/index.tsx
+++ src/components/Asset/AssetContent/StreamSubs/index.tsx
@@ -2,7 +2,6 @@
 import { useAsset } from '@context/Asset'
 import React from 'react'
 import styles from './index.module.css'
-import Copy from '@shared/atoms/Copy'
 import { LoggerInstance } from '@oceanprotocol/lib'
 import { accountTruncate, endpointTruncate } from '@utils/web3'
 import Input from '@shared/FormInput'
@@ -28,35 +27,4 @@ export default function StreamSubs() {
       </section>
     </div>
   )
-
-  // return (
-  //   <footer className={styles.stats}>
-  //     {!asset ||
-  //       !asset?.accessDetails ||
-  //       !asset?.accessDetails?.validOrderTx ? (
-  //       ''
-  //     ) : (
-  //       <>
-  //         {asset?.metadata?.type === 'datastream' && (
-  //           <div>
-  //             <label className={styles.label}>Datastream EndPoint</label>
-  //             <div>
-  //               <section className={styles.endpoint}>
-  //                 <Input
-  //                   type="text"
-  //                   value={endpointFormatted}
-  //                   // readOnly={true}
-  //                   onChange={() => { }}
-  //                 // disabled={true}
-  //                 // prefix={accountTruncate(`${asset.services[0].streamFiles}`)}
-  //                 />
-
-  //               </section>
-  //             </div>
-  //           </div>
-  //         )}
-  //       </>
-  //     )}
-  //   </footer>
-  // )
 }
